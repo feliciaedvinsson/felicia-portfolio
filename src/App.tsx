6da8ef8eb45101013524af3fcd0d3bc947e02189
feliciaedvinsson <158043711+feliciaedvinsson@users.noreply.github.com>
2026-09-19 21:19:06 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/felicia-s-digital-canvas">
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
