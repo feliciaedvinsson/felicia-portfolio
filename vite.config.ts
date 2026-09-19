e368d2e7860d0db397511da15f8f8d04c759c3ce
feliciaedvinsson <158043711+feliciaedvinsson@users.noreply.github.com>
2026-09-19 21:43:00 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/felicia-s-digital-canvas/",
+  base: "/felicia-portfolio/",
   
   server: {
     host: "::",
